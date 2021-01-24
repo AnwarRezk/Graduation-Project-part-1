894150760c4e43fbba25751b4cdadbc6f3e81bcd
Anwar <anwarrezk77@gmail.com>
2021-01-24 21:54:47 +0200

    First Page Done

--- movie_rating/movie_rating/urls.py
+++ movie_rating/movie_rating/urls.py
@@ -1,4 +1,4 @@
-"""movie_rating URL Configuration
+"""MiniSite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -14,9 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from login_register import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('login/', views.LoginPage, name='login'),
+    path('register/', views.RegisterPage, name='register'),
+    path('logout/', views.LogoutUser, name='logout'),
     path('', include('rating.urls')),
 ]
--- movie_rating/users/views.py
+++ movie_rating/users/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+@login_required(login_url='login')  #if user not logged in, redirect to login page
+def home(request):
+    return render(request, 'users/home.html')
+
+def LoginPage(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+    else:
+        if request.method == "POST":
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('/')
+            else: #something went wrong
+                messages.info(request,'Username/Password is incorrect')
+
+        context = {}
+        return render(request, 'users/login.html', context)
+
+def RegisterPage(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+    else:
+        form = UserForm()
+        if request.method == "POST":
+            form = UserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, 'Account created for ' + user)
+                return redirect("login")
+        context = {'form': form}
+        return render(request, 'users/register.html', context)
+
+def LogoutUser(request):
+    logout(request)
+    return redirect('login')
