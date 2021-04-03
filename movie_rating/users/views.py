e37101a97af3368d631cd91633571e2ec359e1ac
ziadsherif3 <ziadsherif3@gmail.com>
2021-04-03 23:23:52 +0200

    Removed home view from users app

--- movie_rating/users/views.py
+++ movie_rating/users/views.py
@@ -6,10 +6,6 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
-@login_required  #if user not logged in, redirect to login page
-def home(request):
-    return render(request, 'users/home.html')
-
 def LoginPage(request):
     if request.user.is_authenticated:
         return redirect('/')
