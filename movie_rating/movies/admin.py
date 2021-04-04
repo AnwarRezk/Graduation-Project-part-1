64622f0a32115fc075ed254bbfa07f0b681a4d4b
MahmoudAlyy <mahmoudabdelrazek2112@gmail.com>
2021-04-04 16:10:46 +0200

    remoced from admin

--- movie_rating/movies/admin.py
+++ movie_rating/movies/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 
-from .models import Movie,MovieInfo,User_Rating,Actor
+from .models import Movie,MovieInfo,User_Rating
 # Register your models here.
 
 admin.site.register(Movie)
-admin.site.register(Actor)
 
 admin.site.register(MovieInfo)
 admin.site.register(User_Rating)
